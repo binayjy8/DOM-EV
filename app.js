aa906715dceb2ba0c610503eaf724bcb71cd4403
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-02 13:47:21 +0530

    code added

--- app.js
+++ app.js
@@ -18,24 +18,31 @@
 //     alert("My Village");
 // }
 
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function (){
+//     let h3 = document.querySelector("h3");
+//     let randomColor = getRandomColor();
+//     h3.innerText = randomColor;
+
+//     let div = document.querySelector("div");
+//     div.style.backgroundColor = randomColor;
+//     console.log("color updated");
+// });
+
+
+// function getRandomColor() {
+//     let red = Math.floor(Math.random() * 255);
+//     let green = Math.floor(Math.random() * 255);
+//     let blue = Math.floor(Math.random() * 255);
+
+//     let color = `rgb(${red}, ${green}, ${blue})`;
+//     return color;
+// }
+
 let btn = document.querySelector("button");
 
-btn.addEventListener("click", function (){
-    let h3 = document.querySelector("h3");
-    let randomColor = getRandomColor();
-    h3.innerText = randomColor;
-
-    let div = document.querySelector("div");
-    div.style.backgroundColor = randomColor;
-    console.log("color updated");
+btn.addEventListener("click", function (event) {
+    console.log(event);
+    console.log("button was clicked");
 });
-
-
-function getRandomColor() {
-    let red = Math.floor(Math.random() * 255);
-    let green = Math.floor(Math.random() * 255);
-    let blue = Math.floor(Math.random() * 255);
-
-    let color = `rgb(${red}, ${green}, ${blue})`;
-    return color;
-}
