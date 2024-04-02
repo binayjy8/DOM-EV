5802374d41c41719a84f486f4e11994990a2f943
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-02 06:47:17 +0530

    code added

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let btn = document.querySelector("button");
 console.dir(btn);
 
-btn.onclick = function () {
-    console.log("button was clicked");
+// btn.onclick = function () {
+//     console.log("button was clicked");
+// }
+
+function sayHello() {
+    alert("Hi Baby");
 }
+
+btn.onclick = sayHello;
