1b2dab0ac6890115d6a86b052ff6ed3ee01c3924
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-02 11:40:01 +0530

    code added

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 let btns = document.querySelectorAll("button");
 
 for(btn of btns) {
-    btn.onclick = sayHello;
-    btn.onmouseenter = function () {
-        console.log("you enter a button");
-    }
+    // btn.onclick = sayHello;
+    // btn.onmouseenter = function () {
+    //     console.log("you enter a button");
+    // }
+
+    btn.addEventListener("click", sayHello);
+    btn.addEventListener("click", sayName);
 }
 
 function sayHello() {
